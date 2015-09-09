0d948d2feeb6a68da39b2ae4fcf821b169cbadc2
Jon Bartlett <mail@jaybe.net>
2015-09-09 15:18:45 +1000

    initial test framework

--- test/test_pvoutput.rb
+++ test/test_pvoutput.rb
@@ -3,7 +3,7 @@ require './lib/pvoutput'
 
 class TestPvoutput < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Pvoutput::VERSION
+    refute_nil ::PVOutput::VERSION
   end
 
 #  def test_it_does_something_useful
